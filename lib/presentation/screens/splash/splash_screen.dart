e25005facebf07739048b064985939dde23f01c2
Ibrahim-Amin-code <ibrahimamin081@gmail.com>
2022-03-16 18:16:55 +0200

    Duration

--- lib/presentation/screens/splash/splash_screen.dart
+++ lib/presentation/screens/splash/splash_screen.dart
@@ -14,7 +14,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        const Duration(seconds: 5),
+        const Duration(seconds: 3),
         () => Navigator.of(context).pushReplacement(MaterialPageRoute(
             builder: (BuildContext context) => OnBoardScreen())));
     // getScreen();
